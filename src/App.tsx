6c735490c0d731e9a671583695e88c0d1353ea2e
Mat Ivan Arquero <mativanarquero@gmail.com>
2023-03-25 23:01:52 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HomePage from "./components/Homepage";
+import StakingApp from "./components/StakingApp";
+import NavMenu from "./components/NavMenu";
+
+const App = () => {
+  return (
+    <Router>
+      <NavMenu />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/staking" component={StakingApp} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
